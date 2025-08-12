6ec28f6f37fb8089b7997d30105014cc42d1e2e9
VamsiKrishna0101 <153058517+VamsiKrishna0101@users.noreply.github.com>
2025-08-12 07:20:39 +0530

    Update ProtectedRoute.jsx

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -5,22 +5,15 @@ import { AuthContext } from '../context/Authcontext';
 export default function ProtectedRoute({ role, children }) {
   const { user } = useContext(AuthContext);
 
-  // While user is being loaded (optional)
   if (user === null) {
-    // You can return a loader here if you want
     return <div>Loading...</div>;
   }
-
   if (!user) {
-    // Not logged in - redirect to login page
     return <Navigate to="/login" replace />;
   }
 
   if (role && user.role !== role) {
-    // Logged in but not authorized for this route
     return <Navigate to="/" replace />;
   }
-
-  // Authorized - render children
   return children;
 }
